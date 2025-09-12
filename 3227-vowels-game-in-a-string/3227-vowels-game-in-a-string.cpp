fc81efaa6674c942d42f77f1bc783598bfa3e172
Merazz <83541936+MerazMz@users.noreply.github.com>
2025-09-12 19:11:50 +0530

    Time: 12 ms (28.92%), Space: 19 MB (79.48%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -1,12 +1,5 @@
 class Solution {
 public:
-    bool isVowel(char c){
-        c=tolower(c);
-        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
-            return true;
-        }
-        return false;
-    }
     bool doesAliceWin(string s) {
         return s.find_first_of("aeiou") != string::npos;
     }
